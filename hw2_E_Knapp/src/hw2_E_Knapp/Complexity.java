b49e6657e01e77f4ce2b956a944419fdb27a7ab1
Eric Knapp <73511389+E-Knapp1@users.noreply.github.com>
2021-04-15 14:23:17 -0400

    hw 2

---
+++ hw2_E_Knapp/src/hw2_E_Knapp/Complexity.java
@@ -0,0 +1,122 @@
+package hw2_E_Knapp;
+
+public class Complexity {
+
+	// method1 -- time complexity of O(n^2) 
+	public static void method1(int n) {
+		int i;
+		int y;
+		int c = 0;
+		
+		for(i = 0; i < n; ++i) {
+			for(y = 0; y < n; ++y) {
+				System.out.println("opertaions: " + c);
+				++c; 
+		}
+	}
+}
+	// method2 -- time complexity of 0(n^3)
+	public static void method2(int n) {
+		int i;
+		int x;
+		int y;
+		int c = 0;
+		
+		for (i = 0; i < n; ++i) {
+			for (x = 0; x < n; ++x) {
+				for (y = 0; y < n; ++y) {
+					System.out.println("operations: " + c);
+					++c;
+				}
+			}
+		}
+	}
+	
+	// method3 -- time complexity of 0(log n)
+	public static void method3(int n) {
+		int i;
+		int c = 0;
+		
+		for (i = 1; i <= n; i = i * 2) {
+			System.out.println("operations: " + c);
+			++c;
+		}
+	}
+	
+	// method4 -- time complexity of 0 (n log n)
+	public static void method4(int n) {
+		int i;
+		int y;
+		int c = 0;
+		
+		for (i = 0; i < n; ++i) {
+			for (y = 1; y <= n; y = y * 2) {
+				System.out.println("operations: " + c);
+				++c;
+			}
+		}
+	}
+	
+	// method5 -- time complexity of 0(log log n)
+	public static void method5(int n) {
+		int i;
+		int c = 0;
+		
+		for(i = 2; i < n; i = i * i) {
+			System.out.println("operations: " + c);
+			++c;
+		}	
+	}
+	
+	
+	
+	
+	// method 6 -- time complexity of O(2^n)
+	public static int method6(int n) {
+		int i;
+		int c = 0;
+		
+		for (i = 1; i <= Math.pow(2, n); ++i) {
+			System.out.println("operations: " + c);
+			++c;
+		
+		}
+		return n;
+	}
+	
+
+	// testing methods with integers as input
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number1 = 2;
+		int number2 = 20;
+
+		
+		System.out.println("method 1 ------------------ \n");
+		method1(number1);
+		System.out.println();
+		
+		System.out.println("method 2 ------------------ \n");
+		method2(number1);
+		System.out.println();
+		
+		System.out.println("method 3 ------------------ \n");
+		method3(number2);
+		System.out.println();
+		
+		System.out.println("method 4 -------------------  \n");
+		method4(number2);
+		System.out.println();
+		
+		System.out.println("method 5 ------------------- \n");
+	    method5(number2);
+		System.out.println();
+		
+		System.out.println("method 6 -------------------\n");
+	    method5(number2);
+		System.out.println();
+		
+	}
+
+}
